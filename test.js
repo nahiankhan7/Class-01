1cd4715d08c2c95e5dd02e0e66302df05af44e7b
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-03 20:11:13 +0600

    add to cart updated

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ const total = document.getElementById("total");
 
 const checkOutButton = document.getElementById("checkOut");
 
-function changeProductQuantity(isIncrease) {
+function changeProductQuantity1(isIncrease) {
   let quantity1 = parseInt(quantityInput1?.value) ?? 0;
   let quantityNewCount1 = quantity1;
   if (isIncrease == true) {
@@ -26,32 +26,36 @@ function changeProductQuantity(isIncrease) {
   quantityInput1.value = quantityNewCount1;
   const countPrice = quantityNewCount1 * 50;
   price1.innerText = countPrice;
+
+  calculateTotal();
 }
 
-// function changeProductQuantity1(isIncrease) {
-//   let quantity1 = parseInt(quantityInput1?.value) ?? 0;
-//   let quantityNewCount1 = quantity1;
-//   if (isIncrease == true) {
-//     quantityNewCount1 = quantity1 + 1;
-//   }
-//   if (isIncrease == false && quantity1 > 0) {
-//     quantityNewCount1 = quantity1 - 1;
-//   }
-//   quantityInput1.value = quantityNewCount1;
-//   const countPrice = quantityNewCount1 * 50;
-//   price1.innerText = countPrice;
-// }
+function changeProductQuantity2(isIncrease) {
+  let quantity2 = parseInt(quantityInput2?.value) ?? 0;
+  let quantityNewCount2 = quantity2;
+  if (isIncrease == true) {
+    quantityNewCount2 = quantity2 + 1;
+  }
+  if (isIncrease == false && quantity2 > 0) {
+    quantityNewCount2 = quantity2 - 1;
+  }
+  quantityInput2.value = quantityNewCount2;
+  const countPrice2 = quantityNewCount2 * 15;
+  price2.innerText = countPrice2;
 
-// function changeProductQuantity2(isIncrease) {
-//   let quantity2 = parseInt(quantityInput2?.value) ?? 0;
-//   let quantityNewCount2 = quantity2;
-//   if (isIncrease == true) {
-//     quantityNewCount2 = quantity2 + 1;
-//   }
-//   if (isIncrease == false && quantity2 > 0) {
-//     quantityNewCount2 = quantity2 - 1;
-//   }
-//   quantityInput2.value = quantityNewCount2;
-//   const countPrice2 = quantityNewCount2 * 15;
-//   price2.innerText = countPrice2;
-// }
+  calculateTotal();
+}
+
+function calculateTotal() {
+  const vapeCount = parseInt(quantityInput1.value);
+  const tankCount = parseInt(quantityInput2.value);
+
+  const totalPrice = vapeCount * 50 + tankCount * 15;
+  subTotal.innerText = "$ " + totalPrice;
+
+  const taxAmount = totalPrice * 0.05;
+  tax.innerText = "$ " + taxAmount;
+
+  const grandTotal = totalPrice + taxAmount;
+  total.innerText = "$ " + grandTotal;
+}
